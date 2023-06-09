6e9995e56ffce33cc9acbab7441de7bcd8325827
Alec <allecc98@gmail.com>
2023-06-09 01:40:31 -0400

    node tutorial

--- Nodejs_tutorial/server.js
+++ Nodejs_tutorial/server.js
@@ -53,17 +53,34 @@ const fs = require('fs');
 
 const PORT = process.env.PORT || 3000;
 
-const server = http
-    .createServer((req, res) => {
-        res.statusCode = 200;
-        res.setHeader("Content-Type", 'text/html')
-        fs.readFile("./index.html", (err, data) => {
-            if (err){
-                console.error(err);
-                res.end();
-            } else {
-                res.end(data);
-            }
-        });
-    })
+const server = http.createServer((req, res) => {
+    console.log(req.url, req.method);
+
+    res.setHeader("Content-Type", 'text/html')
+
+    let path = './';
+    switch(req.url) {
+        case '/':
+            path += 'index.html';
+            res.statusCode = 200;
+            break;
+        case '/about':
+            path += 'about.html';
+            res.statusCode = 200;
+            break;
+        default:
+            path += '404.html';
+            res.statusCode = 404;
+            break;    
+    }
+
+    fs.readFile(path, (err, data) => {
+        if (err){
+            console.error(err);
+            res.end();
+        } else {
+            res.end(data);
+        }
+    });
+})
 .listen(PORT, () => console.log(`Server listening on ${PORT}`));
